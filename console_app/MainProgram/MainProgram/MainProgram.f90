1f32e747417d041e48c840ed97d1d67b2a0a46e1
Nguyen Vinh Phuc <phuc.vinh@outlook.com>
2018-08-20 08:49:41 +0700

    hello word program

---
+++ console_app/MainProgram/MainProgram/MainProgram.f90
@@ -0,0 +1,24 @@
+!  MainProgram.f90 
+!
+!  FUNCTIONS:
+!  MainProgram - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: MainProgram
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program MainProgram
+
+    implicit none
+
+    ! Variables
+
+    ! Body of MainProgram
+    print *, 'Hello World'
+    
+    end program MainProgram
